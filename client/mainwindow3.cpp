b8ddb151947da561bc2675355f834406b59bb714
Toomas Uudisaru <toomas.uudisaru@aktors.ee>
2017-08-26 22:26:13 +0300

    Sidebar navigation
    Signed-off-by: Toomas Uudisaru <toomas.uudisaru@aktors.ee>

--- client/mainwindow3.cpp
+++ client/mainwindow3.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow3.h"
 #include "ui_mainwindow3.h"
 
+#include <QDebug>
+#include <QFontDatabase>
+
 MainWindow3::MainWindow3(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MainWindow3)
 {
     ui->setupUi(this);
+    qDebug() << "Look up fonts";
+    int idRegular = QFontDatabase::addApplicationFont(":/fonts/OpenSans-Regular.ttf");
+    int idSemiBold = QFontDatabase::addApplicationFont(":/fonts/OpenSans-SemiBold.ttf");
+
+    qDebug() << "Create fonts";
+    QFont regular(QFontDatabase::applicationFontFamilies(idRegular).at(0));
+    QFont semiBold(QFontDatabase::applicationFontFamilies(idSemiBold).at(0));
+    qDebug() << "Set fonts";
+    ui->cryptoLabel->setFont(regular);
+    ui->signatureLabel->setFont(semiBold);
+    ui->myEidLabel->setFont(regular);
 }
 
 MainWindow3::~MainWindow3()
