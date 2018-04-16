e59049b2eb51ad7e845156ab4a063008da4bf261
Toomas Uudisaru <toomas@macaroni.local>
2018-04-16 08:34:37 +0300

    Update macOS build scripts for Qt 5.10/XCode 9.3
    Signed-off-by: Toomas Uudisaru <toomas.uudisaru@aktors.ee>

--- prepare_osx_build_environment.sh
+++ prepare_osx_build_environment.sh
@@ -61,9 +61,11 @@ if [ -z "$OPENSSL_PATH" ] ; then
 fi
 qt_ver_parts=( ${QT_VER//./ } )
 QT_MINOR="${qt_ver_parts[0]}.${qt_ver_parts[1]}"
-
 QT_PATH=${BUILD_PATH}/Qt-${QT_VER}-OpenSSL
-
+QT_SRCSPEC="opensource"
+if [[ "$QT_MINOR" = "5.10" ]] ; then
+    QT_SRCSPEC="everywhere"
+fi
 
 GREY='\033[0;37m'
 ORANGE='\033[0;33m'
@@ -79,31 +81,34 @@ SCRIPT_PATH=$(dirname $(realpath "$0") )
 if [[ "$REBUILD" = true || ! -d ${QT_PATH} ]] ; then
     echo -e "\n${ORANGE}##### Building Qt #####${RESET}\n"
     mkdir -p ${BUILD_PATH} && cd ${BUILD_PATH}
-    curl -O -L http://download.qt.io/official_releases/qt/${QT_MINOR}/${QT_VER}/submodules/qtbase-opensource-src-${QT_VER}.tar.xz
-    tar xf qtbase-opensource-src-${QT_VER}.tar.xz
-    cd qtbase-opensource-src-${QT_VER}
+    curl -O -L http://download.qt.io/official_releases/qt/${QT_MINOR}/${QT_VER}/submodules/qtbase-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
+    tar xf qtbase-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
+    cd qtbase-${QT_SRCSPEC}-src-${QT_VER}
     CLANG_MAJOR_VER=$(clang --version | grep LLVM | egrep -o "version ([0-9]{1}\.)" | cut -d ' ' -f 2)
     if [[ "$QT_VER" = "5.9.1" && "$CLANG_MAJOR_VER" = "9." ]] ; then
         patch -Np1 -i $SCRIPT_PATH/patches/qt-5.9.1-xcode-9.patch
     fi
+    if [[ "$CLANG_MAJOR_VER" = "9." ]] ; then
+        patch -Np1 -i $SCRIPT_PATH/patches/qt-5.9.5.patch
+    fi
     ./configure -prefix ${QT_PATH} -opensource -nomake tests -nomake examples -no-securetransport -openssl-linked -confirm-license -I /usr/local/opt/openssl/include -L /usr/local/opt/openssl/lib
     make
     make install
-    rm -rf ${BUILD_PATH}/qtbase-opensource-src-${QT_VER}
-    rm ${BUILD_PATH}/qtbase-opensource-src-${QT_VER}.tar.xz
+    rm -rf ${BUILD_PATH}/qtbase-${QT_SRCSPEC}-src-${QT_VER}
+    rm ${BUILD_PATH}/qtbase-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
 
     cd ${BUILD_PATH}
-    curl -O -L http://download.qt.io/official_releases/qt/${QT_MINOR}/${QT_VER}/submodules/qtsvg-opensource-src-${QT_VER}.tar.xz
-    tar xf qtsvg-opensource-src-${QT_VER}.tar.xz
-    cd qtsvg-opensource-src-${QT_VER}
+    curl -O -L http://download.qt.io/official_releases/qt/${QT_MINOR}/${QT_VER}/submodules/qtsvg-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
+    tar xf qtsvg-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
+    cd qtsvg-${QT_SRCSPEC}-src-${QT_VER}
     "${QT_PATH}"/bin/qmake
     make
     make install
 
     cd ${BUILD_PATH}
-    curl -O -L http://download.qt.io/official_releases/qt/${QT_MINOR}/${QT_VER}/submodules/qttools-opensource-src-${QT_VER}.tar.xz
-    tar xf qttools-opensource-src-${QT_VER}.tar.xz
-    cd qttools-opensource-src-${QT_VER}
+    curl -O -L http://download.qt.io/official_releases/qt/${QT_MINOR}/${QT_VER}/submodules/qttools-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
+    tar xf qttools-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
+    cd qttools-${QT_SRCSPEC}-src-${QT_VER}
     "${QT_PATH}"/bin/qmake
     make
     make install
