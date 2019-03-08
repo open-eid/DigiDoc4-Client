0a873a5b21ce9287e0a38cfd37510dd1077b75da
Raul Metsma <raul@metsma.ee>
2019-03-08 11:45:33 +0200

    Apple store rules do not allow user LDAP.framework (#466)
    IB-5774
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- prepare_osx_build_environment.sh
+++ prepare_osx_build_environment.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 # Requires xcode and cmake, also OpenSSL has to be installed via homebrew.
 
-######### Versions of libraries/frameworks to be compiled
-QT_VER="5.9.7"
-CMAKE_OSX_DEPLOYMENT_TARGET="10.10.5"
-#########
-
-
 set -e
 
+######### Versions of libraries/frameworks to be compiled
+QT_VER="5.9.7"
+OPENLDAP_VER="2.4.47"
 REBUILD=false
 BUILD_PATH=~/cmake_builds
-PROG=$0
-TARGETS=""
+OPENSSL_PATH="/usr/local/opt/openssl"
+: ${MACOSX_DEPLOYMENT_TARGET:="10.11"}
+export MACOSX_DEPLOYMENT_TARGET
 
 while [[ $# -gt 0 ]]
 do
@@ -30,21 +28,27 @@ do
         QT_VER="$2"
         shift
         ;;
+    -l|--openldap)
+        OPENLDAP_VER="$2"
+        shift
+        ;;
     -r|--rebuild)
         REBUILD=true
         ;;
     -h|--help)
         echo "Build Qt for Digidoc4 client"
         echo ""
-        echo "Usage: $PROG [-r|--rebuild] [-p build-path] [-h|--help] [-q|--qt qt-version]"
+        echo "Usage: $0 [-r|--rebuild] [-p build-path] [-h|--help] [-q|--qt qt-version] [-l|--openldap openldap-version]"
         echo ""
         echo "Options:"
         echo "  -o or --openssl openssl-path:"
-        echo "     OpenSSL path; default found from homebrew"
+        echo "     OpenSSL path; default found from homebrew ${OPENSSL_PATH}"
         echo "  -p or --path build-path"
-        echo "     folder where the dependencies should be built; default ~/cmake_builds"
+        echo "     folder where the dependencies should be built; default ${BUILD_PATH}"
         echo "  -q or --qt qt-version:"
-        echo "     Specific version of Qt to build; default $QT_VER "
+        echo "     Specific version of Qt to build; default ${QT_VER} "
+        echo "  -l or --openldap openldap-version:"
+        echo "     Specific version of OpenLDAP to build; default ${OPENLDAP_VER} "
         echo "  -r or --rebuild:"
         echo "     Rebuild even if dependency is already built"
         echo " "
@@ -56,30 +60,22 @@ do
     shift # past argument or value
 done
 
-if [ -z "$OPENSSL_PATH" ] ; then
-    OPENSSL_PATH=`ls -d /usr/local/Cellar/openssl/1.0.* | tail -n 1`
-fi
-qt_ver_parts=( ${QT_VER//./ } )
-QT_MINOR="${qt_ver_parts[0]}.${qt_ver_parts[1]}"
 QT_PATH=${BUILD_PATH}/Qt-${QT_VER}-OpenSSL
-QT_SRCSPEC="opensource"
-if [[ "$QT_MINOR" = "5.10" ]] ; then
-    QT_SRCSPEC="everywhere"
-fi
-
+OPENLDAP_PATH=${BUILD_PATH}/OpenLDAP
 GREY='\033[0;37m'
 ORANGE='\033[0;33m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
-realpath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
-SCRIPT_PATH=$(dirname $(realpath "$0") )
-
-
 if [[ "$REBUILD" = true || ! -d ${QT_PATH} ]] ; then
-    echo -e "\n${ORANGE}##### Building Qt #####${RESET}\n"
+    qt_ver_parts=( ${QT_VER//./ } )
+    QT_MINOR="${qt_ver_parts[0]}.${qt_ver_parts[1]}"
+    QT_SRCSPEC="opensource"
+    if [[ "$QT_MINOR" = "5.10" ]] ; then
+        QT_SRCSPEC="everywhere"
+    fi
+
+    echo -e "\n${ORANGE}##### Building Qt ${QT_VER} ${QT_PATH} #####${RESET}\n"
     mkdir -p ${BUILD_PATH} && cd ${BUILD_PATH}
     curl -O -L http://download.qt.io/official_releases/qt/${QT_MINOR}/${QT_VER}/submodules/qtbase-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
     tar xf qtbase-${QT_SRCSPEC}-src-${QT_VER}.tar.xz
@@ -107,4 +103,16 @@ if [[ "$REBUILD" = true || ! -d ${QT_PATH} ]] ; then
     make install
 else
     echo -e "\n${GREY}  Qt not built${RESET}"
+fi
+
+if [[ "$REBUILD" = true || ! -d ${OPENLDAP_PATH} ]] ; then
+    echo -e "\n${ORANGE}##### Building OpenLDAP ${OPENLDAP_VER} ${OPENLDAP_PATH} #####${RESET}\n"
+    curl -O -L http://mirror.eu.oneandone.net/software/openldap/openldap-release/openldap-${OPENLDAP_VER}.tgz
+    tar xf openldap-${OPENLDAP_VER}.tgz
+    cd openldap-${OPENLDAP_VER}
+    LDFLAGS="-L${OPENSSL_PATH}/lib" CPPFLAGS="-I${OPENSSL_PATH}/include" ./configure -prefix ${OPENLDAP_PATH} --disable-shared --disable-slapd --with-tls=openssl --without-cyrus-sasl
+    make
+    make install
+else
+    echo -e "\n${GREY}  OpenLDAP not built${RESET}"
 fi
