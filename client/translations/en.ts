1be37da27f70f9dbf331a5acd5b7c48e4a5bd76f
Raul Metsma <raul@metsma.ee>
2019-12-13 12:06:22 +0200

    Show more verbose error (#606)
    IB-5988
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- client/translations/en.ts
+++ client/translations/en.ts
@@ -809,6 +809,10 @@ The Information System Authority does not retain information regarding the files
         <source>Please check your computer time. &lt;a href=&apos;https://id.ee/index.php?id=39513&apos;&gt;Additional information&lt;/a&gt;</source>
         <translation>Please check your computer time. &lt;a href=&apos;https://id.ee/index.php?id=39513&apos;&gt;Additional information&lt;/a&gt;</translation>
     </message>
+    <message>
+        <source>The timestamp added to the signature must be taken before validity confirmation</source>
+        <translation>The timestamp added to the signature must be taken before validity confirmation</translation>
+    </message>
 </context>
 <context>
     <name>DocumentModel</name>
