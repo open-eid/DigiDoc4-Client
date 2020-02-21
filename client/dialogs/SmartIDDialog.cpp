6ad3d81453a2fc8cbe9d1d7b085d2a84fc8e582a
Raul Metsma <raul@metsma.ee>
2020-02-21 14:02:54 +0200

    Change Combobox view style
    IB-6250
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- client/dialogs/SmartIDDialog.cpp
+++ client/dialogs/SmartIDDialog.cpp
@@ -1,3 +1,22 @@
+/*
+ * QDigiDoc4
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #include "SmartIDDialog.h"
 #include "ui_SmartIDDialog.h"
 
@@ -61,6 +80,7 @@ SmartIDDialog::SmartIDDialog(QWidget *parent)
 	connect(ui->idCountry, &QComboBox::currentTextChanged, this, enableSign);
 	connect(ui->cbRemember, &QCheckBox::clicked, this, enableSign);
 	enableSign();
+	ui->sign->setFocus();
 }
 
 SmartIDDialog::~SmartIDDialog()
