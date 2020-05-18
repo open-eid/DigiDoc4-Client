fc10794c5b119c936d11deb6158b7c02758332a2
Axel Vasquez <axelv@squez.com.ar>
2020-05-17 22:44:39 -0300

    Update OpenLDAP version
    2.4.49-->2.4.50

--- prepare_osx_build_environment.sh
+++ prepare_osx_build_environment.sh
@@ -5,7 +5,7 @@ set -e
 
 ######### Versions of libraries/frameworks to be compiled
 QT_VER="5.12.8"
-OPENLDAP_VER="2.4.49"
+OPENLDAP_VER="2.4.50"
 REBUILD=false
 BUILD_PATH=~/cmake_builds
 OPENSSL_PATH="/usr/local/opt/openssl"
