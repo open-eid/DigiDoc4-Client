5b10a317b0c112be6b6294d47864db279a0d7b80
Raul Metsma <raul@metsma.ee>
2020-05-31 21:33:32 +0300

    Show different warning to macOS users
    IB-6421
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- client/DigiDoc.cpp
+++ client/DigiDoc.cpp
@@ -528,10 +528,15 @@ bool DigiDoc::open( const QString &file )
 		QWidget *parent = qobject_cast<QWidget *>(QObject::parent());
 		if(parent == nullptr)
 			parent = qApp->activeWindow();
-
+#ifdef Q_OS_MAC 
+		WarningDialog dlg(tr("Signed document in PDF and DDOC format will be transmitted to the Digital Signature Validation Service SiVa to verify the validity of the digital signature. "
+				"Read more information about transmitted data to Digital Signature Validation service from <a href=\"https://id.ee/public/DigiDoc_Andmekaitsetingimused_ENG.pdf\">here</a>. "
+				"Do you want to continue?"), parent);
+#else
 		WarningDialog dlg(tr("Signed document in PDF format will be transmitted to the Digital Signature Validation Service SiVa to verify the validity of the digital signature. "
-			"Read more information about transmitted data to Digital Signature Validation service from <a href=\"https://id.ee/public/DigiDoc_Andmekaitsetingimused_ENG.pdf\">here</a>. "
-			"Do you want to continue?"), parent);
+				"Read more information about transmitted data to Digital Signature Validation service from <a href=\"https://id.ee/public/DigiDoc_Andmekaitsetingimused_ENG.pdf\">here</a>. "
+				"Do you want to continue?"), parent);
+#endif
 		dlg.setCancelText(tr("CANCEL"));
 		dlg.addButton(tr("YES"), ContainerSave);
 		if(dlg.exec() != ContainerSave)
