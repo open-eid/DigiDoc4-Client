e205e713f2cb1fedf400b2931fbf6d4edd414404
Jelena Sanko <jelena@raulwalter.com>
2021-03-19 12:28:03 +0200

    Formatted code, spaces are replaced by tabs
    Signed-off-by: Jelena Sanko <jelena@raulwalter.com>

--- client/dialogs/MobileDialog.cpp
+++ client/dialogs/MobileDialog.cpp
@@ -41,11 +41,10 @@ MobileDialog::MobileDialog(QWidget *parent) :
 	setWindowModality( Qt::ApplicationModal );
 	setFixedSize( size() );
 #if defined (Q_OS_WIN)
-    ui->buttonLayout->setDirection(QBoxLayout::RightToLeft);
+	ui->buttonLayout->setDirection(QBoxLayout::RightToLeft);
 #endif
 
-    connect( ui->sign, &QPushButton::clicked, this, &MobileDialog::accept );
-    connect( ui->sign, &QPushButton::clicked, this, &MobileDialog::accept );
+	connect( ui->sign, &QPushButton::clicked, this, &MobileDialog::accept );
 	connect( ui->cancel, &QPushButton::clicked, this, &MobileDialog::reject );
 	connect( this, &MobileDialog::finished, this, &MobileDialog::close );
 
