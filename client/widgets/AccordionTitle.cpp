3f594291b80d73bc89470b348466c4a3830a3637
Raul Metsma <raul@metsma.ee>
2021-09-05 12:12:35 +0300

    Workaround recent Qt changes to pass mousePressEvent
    IB-7080
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- client/widgets/AccordionTitle.cpp
+++ client/widgets/AccordionTitle.cpp
@@ -26,93 +26,93 @@
 #include <QtGui/QKeyEvent>
 
 AccordionTitle::AccordionTitle(QWidget *parent)
-	: StyledWidget(parent)
-	, ui(new Ui::AccordionTitle)
+    : QLabel(parent)
+    , ui(new Ui::AccordionTitle)
 {
-	ui->setupUi(this);
-	ui->label->setFont( Styles::font( Styles::Condensed, 16 ) );
-	ui->label->setStyleSheet(QStringLiteral("border: none; color: #006EB5;"));
-	ui->icon->load(QStringLiteral(":/images/accordion_arrow_down.svg"));
+    ui->setupUi(this);
+    setFont(Styles::font(Styles::Condensed, 16));
+    ui->icon->load(QStringLiteral(":/images/accordion_arrow_down.svg"));
 }
 
 AccordionTitle::~AccordionTitle()
 {
-	delete ui;
+    delete ui;
 }
 
 bool AccordionTitle::event(QEvent *e)
 {
-	switch(e->type())
-	{
-	case QEvent::MouseButtonRelease:
-		setSectionOpen(!isOpen());
-		break;
-	case QEvent::KeyRelease:
-		if(QKeyEvent *ke = static_cast<QKeyEvent*>(e))
-			if(ke->key() == Qt::Key_Enter || ke->key() == Qt::Key_Space)
-				setSectionOpen(!isOpen());
-		break;
-	default: break;
-	}
-	return StyledWidget::event(e);
+    switch(e->type())
+    {
+    case QEvent::MouseButtonRelease:
+        setSectionOpen(!isOpen());
+        return true;
+    case QEvent::KeyRelease:
+        if(QKeyEvent *ke = static_cast<QKeyEvent*>(e))
+            if(ke->key() == Qt::Key_Enter || ke->key() == Qt::Key_Space)
+                setSectionOpen(!isOpen());
+        break;
+    default: break;
+    }
+    return QLabel::event(e);
 }
 
 void AccordionTitle::init(bool open, const QString &caption, QWidget *content)
 {
-	init(open, caption, caption.toLower(), content);
+    init(open, caption, caption.toLower(), content);
 }
 
 void AccordionTitle::init(bool open, const QString &caption, const QString &accessible, QWidget *content)
 {
-	setText(caption, accessible);
-	this->content = content;
-	setSectionOpen(open);
+    setText(caption, accessible);
+    this->content = content;
+    style = styleSheet();
+    setSectionOpen(open);
 }
 
 bool AccordionTitle::isOpen() const
 {
-	return _isOpen;
+    return _isOpen;
 }
 
 void AccordionTitle::openSection(AccordionTitle *opened)
 {
-	setSectionOpen(this == opened);
+    setSectionOpen(this == opened);
 }
 
 void AccordionTitle::setSectionOpen(bool open)
 {
-	if(_isOpen == open)
-		return;
-	_isOpen = open;
-	if(content)
-		content->setVisible(open && !isHidden());
-	if(open)
-	{
-		ui->label->setStyleSheet(QStringLiteral("border: none; color: #006EB5;"));
-		ui->icon->resize( 12, 6 );
-		ui->icon->move(15, 17);
-		ui->icon->load(QStringLiteral(":/images/accordion_arrow_down.svg"));
-		emit opened(this);
-	}
-	else
-	{
-		ui->label->setStyleSheet(QStringLiteral("border: none; color: #353739;"));
-		ui->icon->resize( 6, 12 );
-		ui->icon->move(18, 14);
-		ui->icon->load(QStringLiteral(":/images/accordion_arrow_right.svg"));
-		emit closed(this);
-	}
+    if(_isOpen == open)
+        return;
+    _isOpen = open;
+    if(content)
+        content->setVisible(open && !isHidden());
+    if(open)
+    {
+        setStyleSheet(style + QStringLiteral("color: #006EB5;"));
+        ui->icon->resize( 12, 6 );
+        ui->icon->move(15, 17);
+        ui->icon->load(QStringLiteral(":/images/accordion_arrow_down.svg"));
+        emit opened(this);
+    }
+    else
+    {
+        setStyleSheet(style + QStringLiteral("color: #353739;"));
+        ui->icon->resize( 6, 12 );
+        ui->icon->move(18, 14);
+        ui->icon->load(QStringLiteral(":/images/accordion_arrow_right.svg"));
+        emit closed(this);
+    }
 }
 
 void AccordionTitle::setText(const QString &caption, const QString &accessible)
 {
-	ui->label->setText(caption);
-	ui->label->setAccessibleName(accessible);
+    QLabel::setText(caption);
+    setAccessibleName(accessible);
 }
 
 void AccordionTitle::setVisible(bool visible)
 {
-	StyledWidget::setVisible(visible);
-	if(content)
-		content->setVisible(visible && isOpen());
+    QLabel::setVisible(visible);
+    if(content)
+        content->setVisible(visible && isOpen());
 }
