695675d51be62ea83ed81f15a0d713eb7a43aa59
Raul Metsma <raul@metsma.ee>
2022-01-13 13:44:54 +0200

    Make hyper link to button (#1008)
    IB-7115
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- client/widgets/WarningItem.cpp
+++ client/widgets/WarningItem.cpp
@@ -20,15 +20,13 @@
 #include "WarningItem.h"
 #include "ui_WarningItem.h"
 
-#include "MainWindow.h"
 #include "Styles.h"
 #include "VerifyCert.h"
 
+#include <QtGui/QTextDocument>
 
-WarningText::WarningText(QString text, QString details, int page)
+WarningText::WarningText(QString text)
 	: text(std::move(text))
-	, details(std::move(details))
-	, page(page)
 {}
 
 WarningText::WarningText(ria::qdigidoc4::WarningType warningType, int counter)
@@ -45,7 +43,9 @@ WarningItem::WarningItem(WarningText warningText, QWidget *parent)
 	ui->warningText->setFont(Styles::font(Styles::Regular, 14));
 	ui->warningAction->setFont(Styles::font(Styles::Regular, 14, QFont::Bold));
 	lookupWarning();
-	connect(ui->warningAction, &QLabel::linkActivated, this, &WarningItem::linkActivated);
+	connect(ui->warningAction, &QPushButton::clicked, this, [this] {
+		Q_EMIT WarningItem::linkActivated(ui->warningAction->property("link").toString());
+	});
 }
 
 WarningItem::~WarningItem()
@@ -68,22 +68,21 @@ void WarningItem::changeEvent(QEvent* event)
 	QWidget::changeEvent(event);
 }
 
-static QString getLink(const QString& text, const QString& url)
-{
-	return QStringLiteral("<a href=\"%1\"><span style=\"color: #006EB5; text-decoration: none;\">%2</span></a>.")
-			.arg(url, text);
-}
 void WarningItem::lookupWarning()
 {
+	auto link = [](const QString &url)
+	{
+		return QStringLiteral("<a href='%1' style='color: #006EB5; text-decoration: none;'>%2</a>.")
+			.arg(url, tr("here"));
+	};
+
 	switch(warnText.warningType)
 	{
 	case ria::qdigidoc4::CertExpiredWarning:
 		warnText.text = QStringLiteral("%1 %2 %3")
 				.arg(tr("Certificates have expired! "),
 					tr("You can find instructions on how to get a new document from "),
-					 ::getLink(tr("here"),
-							   tr("https://www.politsei.ee/en/instructions/applying-for-an-id-card-for-an-adult/")));
-		//make a QLabel behave like a hyperlink
+					link(tr("https://www.politsei.ee/en/instructions/applying-for-an-id-card-for-an-adult/")));
 		ui->warningText->setTextInteractionFlags(Qt::TextBrowserInteraction);
 		ui->warningText->setOpenExternalLinks(true);
 		break;
@@ -91,54 +90,54 @@ void WarningItem::lookupWarning()
 		warnText.text = QStringLiteral("%1 %2 %3")
 				.arg(tr("Certificates expire soon! "),
 					 tr("You can find instructions on how to get a new document from "),
-					 ::getLink(tr("here"),
-							   tr("https://www.politsei.ee/en/instructions/applying-for-an-id-card-for-an-adult/")));
+					 link(tr("https://www.politsei.ee/en/instructions/applying-for-an-id-card-for-an-adult/")));
 		ui->warningText->setTextInteractionFlags(Qt::TextBrowserInteraction);
 		ui->warningText->setOpenExternalLinks(true);
 		break;
 	case ria::qdigidoc4::CertRevokedWarning:
 		warnText.text = tr("Certificates are revoked!");
-		warnText.details = QStringLiteral("<a href='%1'><span style='color:rgb(53, 55, 57)'>%2</span></a>").arg(tr("https://www.id.ee/en/article/the-majority-of-electronically-used-id-cards-were-renewed/"), tr("Additional information").toUpper());
+		warnText.url = tr("https://www.id.ee/en/article/the-majority-of-electronically-used-id-cards-were-renewed/");
+		warnText.details = tr("Additional information").toUpper();
 		break;
 	case ria::qdigidoc4::UnblockPin1Warning:
 		warnText.text = QStringLiteral("%1 %2").arg(VerifyCert::tr("PIN%1 has been blocked because PIN%1 code has been entered incorrectly 3 times.").arg(1), VerifyCert::tr("Unblock to reuse PIN%1.").arg(1));
-		warnText.details = QStringLiteral("<a href='#unblock-PIN1'><span style='color:rgb(53, 55, 57)'>%1</span></a>").arg(VerifyCert::tr("UNBLOCK"));
-		ui->warningAction->setOpenExternalLinks(false);
+		warnText.url = QStringLiteral("#unblock-PIN1");
+		warnText.details = VerifyCert::tr("UNBLOCK");
 		break;
 	case ria::qdigidoc4::UnblockPin2Warning:
 		warnText.text = QStringLiteral("%1 %2").arg(VerifyCert::tr("PIN%1 has been blocked because PIN%1 code has been entered incorrectly 3 times.").arg(2), VerifyCert::tr("Unblock to reuse PIN%1.").arg(2));
-		warnText.details = QStringLiteral("<a href='#unblock-PIN2'><span style='color:rgb(53, 55, 57)'>%1</span></a>").arg(VerifyCert::tr("UNBLOCK"));
-		ui->warningAction->setOpenExternalLinks(false);
+		warnText.url = QStringLiteral("#unblock-PIN2");
+		warnText.details = VerifyCert::tr("UNBLOCK");
 		break;
 	// SignDetails
 	case ria::qdigidoc4::InvalidSignatureWarning:
 		warnText.text = tr("%n signatures are not valid!", nullptr, warnText.counter);
-		warnText.details = QStringLiteral("<a href='%1' style='color: rgb(53, 55, 57)'>%2</a>")
-					.arg(tr("https://www.id.ee/en/article/digital-signing-and-electronic-signatures/"), tr("More information"));
+		warnText.url = tr("https://www.id.ee/en/article/digital-signing-and-electronic-signatures/");
+		warnText.details = tr("More information");
 		warnText.page = ria::qdigidoc4::SignDetails;
 		break;
 	case ria::qdigidoc4::InvalidTimestampWarning:
 		warnText.text = tr("%n timestamps are not valid!", nullptr, warnText.counter);
-		warnText.details = QStringLiteral("<a href='%1' style='color: rgb(53, 55, 57)'>%2</a>")
-					.arg(tr("https://www.id.ee/en/article/digital-signing-and-electronic-signatures/"), tr("More information"));
+		warnText.url = tr("https://www.id.ee/en/article/digital-signing-and-electronic-signatures/");
+		warnText.details = tr("More information");
 		warnText.page = ria::qdigidoc4::SignDetails;
 		break;
 	case ria::qdigidoc4::UnknownSignatureWarning:
 		warnText.text = tr("%n signatures are unknown!", nullptr, warnText.counter);
-		warnText.details = QStringLiteral("<a href='%1' style='color: rgb(53, 55, 57)'>%2</a>")
-					.arg(tr("https://www.id.ee/en/article/digital-signing-and-electronic-signatures/"), tr("More information"));
+		warnText.url = tr("https://www.id.ee/en/article/digital-signing-and-electronic-signatures/");
+		warnText.details = tr("More information");
 		warnText.page = ria::qdigidoc4::SignDetails;
 		break;
 	case ria::qdigidoc4::UnknownTimestampWarning:
 		warnText.text = tr("%n timestamps are unknown!", nullptr, warnText.counter);
-		warnText.details = QStringLiteral("<a href='%1' style='color: rgb(53, 55, 57)'>%2</a>")
-					.arg(tr("https://www.id.ee/en/article/digital-signing-and-electronic-signatures/"), tr("More information"));
+		warnText.url = tr("https://www.id.ee/en/article/digital-signing-and-electronic-signatures/");
+		warnText.details = tr("More information");
 		warnText.page = ria::qdigidoc4::SignDetails;
 		break;
 	case ria::qdigidoc4::UnsupportedDDocWarning:
 		warnText.text = tr("The current file is a DigiDoc container that is not supported officially any longer. You are not allowed to add or remove signatures to this container.");
-		warnText.details = QStringLiteral("<a href='%1' style='color: rgb(53, 55, 57)'>%2</a>")
-					.arg(tr("https://www.id.ee/en/article/digidoc-container-format-life-cycle-2/"), tr("More information"));
+		warnText.url = tr("https://www.id.ee/en/article/digidoc-container-format-life-cycle-2/");
+		warnText.details = tr("More information");
 		warnText.page = ria::qdigidoc4::SignDetails;
 		break;
 	case ria::qdigidoc4::EmptyFileWarning:
@@ -148,8 +147,19 @@ void WarningItem::lookupWarning()
 	default:
 		break;
 	}
+
+	auto setAccessibleName = [](QLabel *label) {
+		QTextDocument doc;
+		doc.setHtml(label->text().toLower());
+		label->setAccessibleName(doc.toPlainText());
+	};
+
 	ui->warningText->setText(warnText.text);
 	ui->warningAction->setText(warnText.details);
+	ui->warningAction->setProperty("link", warnText.url);
+	ui->warningAction->setAccessibleName(warnText.details.toLower());
+	ui->warningAction->setHidden(warnText.details.isEmpty());
+	setAccessibleName(ui->warningText);
 }
 
 ria::qdigidoc4::WarningType WarningItem::warningType() const
