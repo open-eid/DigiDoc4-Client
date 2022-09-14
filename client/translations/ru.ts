6dce1b41e36ade02262fb7d40cbbc2de4d520e84
Kristjan Vaikla <47743732+Counter178@users.noreply.github.com>
2022-09-14 13:11:45 +0300

    Update ru.ts

--- client/translations/ru.ts
+++ client/translations/ru.ts
@@ -77,8 +77,8 @@
         <translation>Личный код недействителен!</translation>
     </message>
     <message>
-        <source>Person or company does not own a valid certificate.&lt;br /&gt;It is necessary to have a valid certificate for encryption.&lt;br /&gt;In case of questions please contact our support via &lt;a href=&quot;https://www.id.ee/en/&quot;&gt;id.ee&lt;/a&gt;.</source>
-        <translation>У человека или предприятия отсутствует действующий сертификат. Для шифрования нужен действующий сертификат. &lt;a href=&apos;https://www.id.ee/ru/artikkel/shifrovanie-i-deshifrovka-dokumentov/&apos;&gt;Дополнительная информация&lt;/a&gt;.</translation>
+        <source>Person or company does not own a valid certificate.&lt;br /&gt;It is necessary to have a valid certificate for encryption.&lt;br /&gt;&lt;a href=&quot;https://www.id.ee/en/article/encryption-and-decryption-of-documents/&quot;&gt;Additional information&lt;/a&gt;.</source>
+        <translation>У человека или предприятия отсутствует действующий сертификат.&lt;br /&gt;Для шифрования нужен действующий сертификат.&lt;br /&gt;&lt;a href=&apos;https://www.id.ee/ru/artikkel/shifrovanie-i-deshifrovka-dokumentov/&apos;&gt;Дополнительная информация&lt;/a&gt;.</translation>
     </message>
     <message>
         <source>Certificates (*.cer *.crt *.pem)</source>
@@ -218,7 +218,7 @@
         <translation>Создайте %1</translation>
     </message>
     <message>
-        <source>Added file(s) exceeds the maximum size limit of the container(120MB).</source>
+        <source>Added file(s) exceeds the maximum size limit of the container (~120MB).&lt;a href=&apos;https://www.id.ee/en/article/encrypting-large-120-mb-files/&apos;&gt;Additional information&lt;/a&gt;.</source>
         <translation>Вложенные файл(ы) превышают размер контейнера безопасности (~120 МБ).&lt;a href=&apos;https://www.id.ee/ru/artikkel/kriptovanie-fajlov-s-bolshim-obemom-120-mb/&apos;&gt;Дополнительная информация&lt;/a&gt;.</translation>
     </message>
     <message>
@@ -1683,14 +1683,14 @@ ID-КАРТОЙ</translation>
         <translation>Фото (*.jpg *.jpeg);;Все файлы (*)</translation>
     </message>
     <message>
-        <source>Open file for signing or verifying</source>
+        <source>Load file from disk for signing or verifying</source>
         <comment>accessible</comment>
-        <translation>Загрузить файл с диска для шифрования или расшифровки</translation>
+        <translation>Загрузить файл с диска для подписания или проверки</translation>
     </message>
     <message>
-        <source>Open file for encryption or decryption</source>
+        <source>Load file from disk for encryption or decryption</source>
         <comment>accessible</comment>
-        <translation>Выберите на диске файл для шифрования или расшифровки</translation>
+        <translation>Загрузить файл с диска для шифрования или расшифровки</translation>
     </message>
     <message>
         <source>Select signature page</source>
@@ -2609,7 +2609,7 @@ and enter Smart-ID PIN2-code.</source>
     </message>
     <message>
         <source>&lt;p&gt;Estonian ID-software is released by Information Systems&apos;s Authority&lt;br /&gt;
-In case of questions please contact our support via &lt;a href=&quot;https://www.id.ee/en/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#006eb5;&quot;&gt;id.ee&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;
+In case of questions please contact our support via &lt;a href=&quot;https://www.id.ee/ru/id-pomoshh/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#006eb5;&quot;&gt;id.ee&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;
 Additional licenses and components</source>
         <translation>&lt;p&gt;Разработчиком программного обеспечения ID-карты является Riigi Infosüsteemi Amet.&lt;br /&gt;
 Если у вас возникнут вопросы, пожалуйста, обратитесь на &lt;a href=&quot;https://www.id.ee/ru/id-pomoshh/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#006eb5;&quot;&gt;id.ee&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;
@@ -2862,7 +2862,7 @@ Additional licenses and components</source>
         <translation>Повреждена подпись или подписанный файл. Данная подпись недействительна.</translation>
     </message>
     <message>
-        <source>Signature status is displayed &quot;unknown&quot; if you don&apos;t have all validity confirmation service certificates and/or certificate authority certificates installed into your computer (&lt;a href=&apos;https://www.id.ee/en/article/digidoc4-klient-viga-36-failed-to-validate-signature-libdigidoc-code-36-message-signers-cert-not-trusted-missing-ca-cert/&apos;&gt;additional information&lt;/a&gt;).</source>
+        <source>Signature status is displayed &quot;unknown&quot; if you don&apos;t have all validity confirmation service certificates and/or certificate authority certificates installed into your computer (&lt;a href=&apos;https://www.id.ee/en/article/digidoc4-client-error-ocsp-responder-is-not-trusted-no-certificate-for-this-responder-in-local-certstore/&apos;&gt;additional information&lt;/a&gt;).</source>
         <translation>Статус подписи отображается как &quot;неизвестный&quot;, если на компьютере не установлены необходимые для проверки подписи сертификаты сертифицирующей организации и/или услуги подтверждения действительности (&lt;a href=&apos;https://www.id.ee/ru/artikkel/digidoc4-klient-oshibka-ocsp-responder-is-not-trusted-no-certificate-for-this-responder-in-local-certstore/&apos;&gt;дополнительная информация&lt;/a&gt;).</translation>
     </message>
     <message>
@@ -3002,8 +3002,8 @@ Additional licenses and components</source>
         <translation>время</translation>
     </message>
     <message>
-        <source>Remove signature %1</source>
-        <translation>Удалить подпись? %1</translation>
+        <source>Remove signature %1?</source>
+        <translation>Удалить подпись %1?</translation>
     </message>
     <message>
         <source>CANCEL</source>
