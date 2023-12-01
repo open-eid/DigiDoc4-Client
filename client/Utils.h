e321e6fa7a1a686c6bd44d1d8ee0272f2cb98896
Raul Metsma <raul@metsma.ee>
2023-12-01 11:06:42 +0200

    Don't use QTimer in dispatchToMain (#1237)
    IB-7904
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- client/Utils.h
+++ client/Utils.h
@@ -49,25 +49,18 @@ namespace {
 	template <typename F, class... Args>
 	inline auto dispatchToMain(F&& function, Args&& ...args) {
 		QEventLoop l;
-		QTimer *timer = new QTimer();
-		timer->moveToThread(qApp->thread());
-		timer->setSingleShot(true);
 		if constexpr (std::is_void_v<std::invoke_result_t<F,Args...>>) {
-			QObject::connect(timer, &QTimer::timeout, timer, [&, function = std::forward<F>(function)] {
+			QMetaObject::invokeMethod(qApp, [&, function = std::forward<F>(function)] {
 				std::invoke(function, args...);
 				l.exit();
-				timer->deleteLater();
-			});
-			QMetaObject::invokeMethod(timer, [timer] { timer->start(0); }, Qt::QueuedConnection);
+			}, Qt::QueuedConnection);
 			l.exec();
 		} else {
 			std::invoke_result_t<F,Args...> result{};
-			QObject::connect(timer, &QTimer::timeout, timer, [&, function = std::forward<F>(function)] {
+			QMetaObject::invokeMethod(qApp, [&, function = std::forward<F>(function)] {
 				result = std::invoke(function, args...);
 				l.exit();
-				timer->deleteLater();
-			});
-			QMetaObject::invokeMethod(timer, [timer] { timer->start(0); }, Qt::QueuedConnection);
+			}, Qt::QueuedConnection);
 			l.exec();
 			return result;
 		}
