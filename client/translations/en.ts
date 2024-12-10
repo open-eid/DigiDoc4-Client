301e903e529847e1e3ad1e06b18b5e8953d56921
Raul Metsma <raul@metsma.ee>
2024-12-10 16:10:55 +0200

    Build windows Arm64 binaries
    IB-6662, IB-8805
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- client/translations/en.ts
+++ client/translations/en.ts
@@ -460,6 +460,17 @@
     </message>
 </context>
 <context>
+    <name>Configuration</name>
+    <message>
+        <source>The configuration file located on the server cannot be validated.</source>
+        <translation>The configuration file located on the server cannot be validated.</translation>
+    </message>
+    <message>
+        <source>Your computer&apos;s configuration file is later than the server has.</source>
+        <translation>Your computer&apos;s configuration file is later than the server has.</translation>
+    </message>
+</context>
+<context>
     <name>ContainerPage</name>
     <message>
         <source>Container:</source>
