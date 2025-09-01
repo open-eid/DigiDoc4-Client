21cd7ed9a3622fdaaf791dc8b7cadbb55911f0d8
Raul Metsma <raul@metsma.ee>
2025-09-01 14:37:33 +0300

    Update Qt 6.9.2 and OpenSSL 3.5.2
    IB-8585
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- prepare_osx_build_environment.sh
+++ prepare_osx_build_environment.sh
@@ -4,8 +4,8 @@
 set -e
 
 ######### Versions of libraries/frameworks to be compiled
-QT_VER="6.9.1"
-OPENSSL_VER="3.5.1"
+QT_VER="6.9.2"
+OPENSSL_VER="3.5.2"
 OPENLDAP_VER="2.6.10"
 REBUILD=false
 BUILD_PATH=~/cmake_builds
