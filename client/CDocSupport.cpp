a4375785abcea27be80d5d88c94eea290dbd028c
Lauris Kaplinski <lauris@raulwalter.com>
2026-09-23 15:37:09 +0300

    Fix CodeQL warning
    Signed-off-by: Lauris Kaplinski <lauris@raulwalter.com>

--- client/CDocSupport.cpp
+++ client/CDocSupport.cpp
@@ -346,9 +346,9 @@ DDNetworkBackend::fetchKey(std::vector<uint8_t> &result, const std::string &url,
 	if(reply->error() != QNetworkReply::NoError && reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt() != 201) {
         if (backend.get()->status != QCryptoBackend::PinOK) {
             // The actual error was in QCryptoBackend
-            libcdoc::result_t result = getDecryptStatus(backend.get()->status);
-            last_error = getLastErrorStr(result);
-            return result;
+            libcdoc::result_t lresult = getDecryptStatus(backend.get()->status);
+            last_error = getLastErrorStr(lresult);
+            return lresult;
         }
         // Probably network problem
 		last_error = reply->errorString().toStdString();
